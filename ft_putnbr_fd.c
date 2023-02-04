4e1c1c382d625fa56c99b85ce60bd6992356f401
PeliAzuh <47522008+krl299@users.noreply.github.com>
2023-02-04 12:47:18 +0100

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -15,11 +15,11 @@
 // Outputs the integer ’n’ to the given file descriptor.
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n <= 2147483647 || n >= -2147483648)
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	if (n <= 2147483647 || n >= -2147483647)
 	{
-		if (n == -2147483648)
-			write(fd, "-2147483648", 11);
-		else if (n < 0)
+		if (n < 0)
 		{
 			write(fd, "-", 1);
 			n *= -1;
